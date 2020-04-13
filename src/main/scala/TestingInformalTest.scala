dc4e41bacf08e74c37258cd28a51f48bd9c7192a
Allen Han <allenjhan@gmail.com>
2020-04-13 11:45:58 -0700

    initial

---
+++ src/main/scala/TestingInformalTest.scala
@@ -0,0 +1,10 @@
+object TestingInformalTest {
+
+  /*
+  The maximum of a list should be the same regardless of order; reverse the list, sort the list, and scrambling the list
+  should not change the maximum.
+  The maximum of maximums of partitions of the list should be equal to the maximum of the list.
+  No element of the list should be larger than the maximum of the list.
+  Every element of the list should be smaller or equal to the maximum of the list.
+   */
+}
